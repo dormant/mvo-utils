8efcadf7434c5f8806ff59ee9d263be80aba8f4d
dormant <rod@dormant.org>
2025-05-12 14:53:27 +0000

    Edited fetchHelis.sh to remove MSS1 fetcgh, which takes ages.

--- fetchHelis.sh
+++ fetchHelis.sh
@@ -11,5 +11,5 @@ then
 else
 	dateHeli=$1
     find /mnt/mvofls2/Seismic_Data/monitoring_data/helicorder_plots_multi -name "*${dateHeli}*" -print0 | xargs -0 cp --target-directory=.
-    find /mnt/mvofls2/Seismic_Data/monitoring_data/helicorder_plots -name "MSS1*${dateHeli}*" -print0 | xargs -0 cp --target-directory=.
+    #find /mnt/mvofls2/Seismic_Data/monitoring_data/helicorder_plots -name "MSS1*${dateHeli}*" -print0 | xargs -0 cp --target-directory=.
 fi
